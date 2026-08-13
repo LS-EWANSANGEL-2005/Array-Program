887a4bdf13e82e8bc5b0f943a110a987ef26a679
Ewans Angel LS <ewansangel2005@gmail.com>
2026-08-13 10:20:06 +0530

    Add files via upload

---
+++ ExampleType2.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class ExampleType2 {
+
+	public static void main(String[] args) {
+		int marks[]= {98,96,45,85,91};
+		int len=marks.length;
+		for(int i=0;i<len;i++) {
+		System.out.println(marks[i]);
+	}
+
+}
+}
