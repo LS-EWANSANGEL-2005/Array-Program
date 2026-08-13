58b599413fdb2e3180d78b69d0e2f62269806ad7
Ewans Angel LS <ewansangel2005@gmail.com>
2026-08-13 10:19:37 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,21 @@
+package Array;
+
+public class Example {
+
+	public static void main(String[] args) {
+		//type 1 using new keyword
+		int size=3;
+		int [] nums = new int[size];
+		//intilize the array value
+		nums[0]=10;
+		nums[1]=20;
+		nums[2]=30;
+		//print array
+		for(int i =0;i<size;i++) {
+			System.out.println(nums[i]);
+		}
+		
+
+	}
+
+}
