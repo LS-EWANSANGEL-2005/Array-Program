b87c6009be12438a35f470e38c04cc277b83b610
Ewans Angel LS <ewansangel2005@gmail.com>
2026-08-18 11:55:55 +0530

    Add files via upload

---
+++ HashMapExample.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class HashMapExample {
+
+    public static void main(String[] args) {
+
+        java.util.HashMap<String, String> map = new java.util.HashMap<>();
+
+        map.put("name", "vinothini");
+        map.put("house no", "1/204");
+        map.put("area", "pavalandhur");
+        map.put("district", "Dharmapuri");
+
+        System.out.println("Original Map: " + map);
+    }
+}
